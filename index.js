68c733a84f0334c37f16fb97ef8390ae4a38b1e0
Ryan Berry <rberry16@hotmail.com>
2022-01-11 19:54:24 -0700

    Went back for touchups to get perfect score

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ Do the following:
 */
 const num = '1999';
 
-//console.log(Number(num));
+
+console.log(Number(num));
 
 
 
@@ -179,7 +180,7 @@ if (computer === 0){
 
 function game(user, computer){
   if (user === computer){
-    return "it's a tie!";
+    return "it's a tie";
   }else if (user === 'rock' && computer === 'scissors'){
     return 'you win!';
   }else if (user === 'rock' && computer === 'paper'){
@@ -260,10 +261,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+if (score >= 90 && score <= 100){
+  return 'you got an A';
+}else if (score >= 80 && score <= 89){
+  return 'you got a B';
+}else if (score >= 70 && score <= 79){
+  return 'you got a C';
+}else if (score >=60 && score <= 69){
+  return 'you got a D';
+}else if (score < 60){
+  return 'you got an F';
 }
-
+}
+console.log(grade(100))
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
